8a69cf6bc0cfe721c83ab226deef28c8c699d3dd
Andrew Wippler <andrew.wippler@gmail.com>
2017-05-05 15:40:25 -0700

    Typo

--- resources/views/households/edit.blade.php
+++ resources/views/households/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <section class="content-header">
         <h1>
-            Prospeect Household
+            Prospect Household
         </h1>
    </section>
    <div class="content">
